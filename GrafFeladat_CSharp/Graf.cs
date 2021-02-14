245fbb166d7af0b294a11fe51a105ee2b0d4aed2
Pleew-HOME <ptl2217@gmail.com>
2021-02-14 17:15:58 +0100

    torles, szelessegibejar, melysegibejar

--- GrafFeladat_CSharp/Graf.cs
+++ GrafFeladat_CSharp/Graf.cs
@@ -25,13 +25,11 @@ namespace GrafFeladat_CSharp
         /// Létehoz egy úgy, N pontú gráfot, élek nélkül.
         /// </summary>
         /// <param name="csucsok">A gráf csúcsainak száma</param>
-        public Graf(int csucsok)
-        {
+        public Graf(int csucsok){
             this.csucsokSzama = csucsok;
 
             // Minden csúcsnak hozzunk létre egy új objektumot
-            for (int i = 0; i < csucsok; i++)
-            {
+            for (int i = 0; i < csucsok; i++){
                 this.csucsok.Add(new Csucs(i));
             }
         }
@@ -43,19 +41,15 @@ namespace GrafFeladat_CSharp
         /// </summary>
         /// <param name="cs1">Az él egyik pontja</param>
         /// <param name="cs2">Az él másik pontja</param>
-        public void Hozzaad(int cs1, int cs2)
-        {
+        public void Hozzaad(int cs1, int cs2){
             if (cs1 < 0 || cs1 >= csucsokSzama ||
-                cs2 < 0 || cs2 >= csucsokSzama)
-            {
+                cs2 < 0 || cs2 >= csucsokSzama){
                 throw new ArgumentOutOfRangeException("Hibas csucs index");
             }
 
             // Ha már szerepel az él, akkor nem kell felvenni
-            foreach (var el in elek)
-            {
-                if (el.Csucs1 == cs1 && el.Csucs2 == cs2)
-                {
+            foreach (var el in elek){
+                if (el.Csucs1 == cs1 && el.Csucs2 == cs2){
                     return;
                 }
             }
@@ -63,20 +57,85 @@ namespace GrafFeladat_CSharp
             elek.Add(new El(cs1, cs2));
             elek.Add(new El(cs2, cs1));
         }
+        public void Torles(int cs1, int cs2){
+            if (cs1 < 0 || cs1 >= csucsokSzama ||
+                cs2 < 0 || cs2 >= csucsokSzama){
+                throw new ArgumentOutOfRangeException("Hibas csucs index");
+            }
 
-        public override string ToString()
-        {
+
+            int i = 0;
+            int ind1 = -1;
+            int ind2 = -1;
+            foreach (var el in elek){
+                if (el.Csucs1 == cs1 && el.Csucs2 == cs2){
+                    ind1 = i;
+                }
+                else if (el.Csucs2 == cs1 && el.Csucs1 == cs2){
+                    ind2 = i;
+                }
+                i++;
+            }
+
+            if (ind2 != -1){
+                elek.RemoveAt(ind2);
+            }
+            if (ind1 != -1){
+                elek.RemoveAt(ind1);
+            }
+
+        }
+        public override string ToString(){
             string str = "Csucsok:\n";
-            foreach (var cs in csucsok)
-            {
+            foreach (var cs in csucsok){
                 str += cs + "\n";
             }
             str += "Elek:\n";
-            foreach (var el in elek)
-            {
+            foreach (var el in elek){
                 str += el + "\n";
             }
             return str;
+        }
+        public void szelessegiBejar(int kezdoPont){
+            List<int> bejar = new List<int>();
+
+            Queue<int> sor = new Queue<int>();
+            sor.Enqueue(kezdoPont);
+            bejar.Add(kezdoPont);
+
+            while (sor.Count > 0){
+                int k = sor.Dequeue();
+
+                Console.WriteLine("A csúcs: " + k);
+
+                foreach (var item in elek){
+                    if (item.Csucs1 == k && !(bejar.Contains(item.Csucs2))){
+                        sor.Enqueue(item.Csucs2);
+                        bejar.Add(item.Csucs2);
+                    }
+                }
+
+            }
+        }
+        public void melysegiBejar(int kezdoPont){
+            List<int> bejar = new List<int>();
+
+            Stack<int> kovetkezok = new Stack<int>();
+            kovetkezok.Push(kezdoPont);
+            bejar.Add(kezdoPont);
+
+            while (kovetkezok.Count > 0){
+                int k = kovetkezok.Pop();
+
+                Console.WriteLine("A csúcs: " + k);
+
+                foreach (var item in elek){
+                    if (item.Csucs1 == k && !(bejar.Contains(item.Csucs2))){
+                        kovetkezok.Push(item.Csucs2);
+                        bejar.Add(item.Csucs2);
+                    }
+                }
+            }
         }
     }
 }
